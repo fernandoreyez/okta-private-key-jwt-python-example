e63b9c093ac0f8e4a41ef460572ff8de4a50173d
Fernando Reyes <fernando.reyes@procore.com>
2023-01-27 18:20:02 -0500

    use http.client versus aiohttp

--- get_access_token.py
+++ get_access_token.py
@@ -1,17 +1,17 @@
-import aiohttp
-import asyncio
+
 import time
 import pprint
 import jwt
 import os
 import dotenv
 import json
+import http.client
 
 pp = pprint.PrettyPrinter(indent=4)
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 dotenv.load_dotenv(dotenv_path)
  
-async def create_private_key_jwt():
+def create_private_key_jwt():
 	client_id = os.environ.get('CLIENT_ID')
 	okta_url = os.environ.get('OKTA_URL')
 	okta_token_endpoint = f'{okta_url}/oauth2/v1/token'
@@ -27,23 +27,28 @@ async def create_private_key_jwt():
 	)
 	return token
 
-async def main():
+def main():
 	okta_url = os.environ.get('OKTA_URL')
-	private_key_jwt = await create_private_key_jwt()
+	private_key_jwt = create_private_key_jwt()
 	scope = os.environ.get('SCOPES')
  
-	data = f'grant_type=client_credentials&scope={scope}&client_assertion_type=urn%3Aietf%3Aparams%3Aoauth%3Aclient-assertion-type%3Ajwt-bearer&client_assertion={private_key_jwt}'
+	payload = f'grant_type=client_credentials&scope={scope}&client_assertion_type=urn%3Aietf%3Aparams%3Aoauth%3Aclient-assertion-type%3Ajwt-bearer&client_assertion={private_key_jwt}'
+
 	headers = {
 		'Accept': 'application/json',
 		'Content-Type': 'application/x-www-form-urlencoded'
 	}
+
+	conn = http.client.HTTPSConnection(okta_url.replace('https://', ''))
+
+	conn.request(
+		'POST',
+		f'/oauth2/v1/token',
+		payload,
+		headers
+		)
+
+	response = conn.getresponse()
+	print(f"code:{response.status}\nrespoonse:{json.loads(response.read())['access_token']}")
  
-	async with aiohttp.ClientSession() as session:
-		async with session.post(okta_url + '/oauth2/v1/token',
-							data=data, headers=headers) as resp:
-			res = await resp.json()
-
-	print('\n///////////////// Access Token:\n')
-	pp.pprint(res)
-
-asyncio.run(main())
+main()
